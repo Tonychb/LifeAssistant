8e1485271fe71578b19dd0cf8a0363c34f7e6546
zhubaoshi <zhubaoshi@zhubaoshideMacBook-Air.local>
2015-05-05 16:07:46 +0800

    Initial commit

---
+++ LifeAssistant/LifeAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifeAssistant
+//
+//  Created by zhubaoshi on 15/5/5.
+//  Copyright (c) 2015年 tonychb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LifeAssistant/LifeAssistant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LifeAssistant
+//
+//  Created by zhubaoshi on 15/5/5.
+//  Copyright (c) 2015年 tonychb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
